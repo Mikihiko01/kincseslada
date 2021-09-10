02dbedcf6b9689a786e50d8b940b1daba6830dd7
Mikihiko01 <tellad.adam@gmail.com>
2021-09-10 16:06:22 +0200

    Kincses Lada

---
+++ src/kincseslada/KincsesLada.java
@@ -0,0 +1,15 @@
+
+package kincseslada;
+
+public class KincsesLada {
+
+    
+    public static void main(String[] args) {
+        
+       String[] ladak = {"Arany", "Ezüst", "Bronz"};
+       String[] modas = {"Én rejtem a kincset", "Nem én rejtem a kincset", "Az arany láda hazudik"};
+       
+        
+    }
+    
+}
